72a8d28091be22eead61843969e4e5e4f108f3a9
Albrigs <natanfernandessantos@protonmail.com>
2020-10-16 15:52:10 -0300

    :zap::construction:Paths
    Agora paths costumizados podem ser adicionados em pasta onde não irão poluir outros scripts de inicialização

--- conf.sh
+++ conf.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#Script só inicia se estiver conectado a internet
+is_online=$(ping -c 1 -q 8.8.8.8 >&/dev/null; echo $?)
+if [ $is_online != 0 ]
+then
+	echo "You are offline, this script will not work."
+	exit
+fi
+
+
 ppa_exists() #remove trecho de loop se ppa já estiver instalado
 {
 	apt policy | grep $1
@@ -12,9 +21,9 @@ sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 241FE6973B765FAE
 
+
 #Repositórios não nativos
 PPAS=("webupd8team/atom")
-
 for e in ${PPAS[@]} 
 do 
 ppa_exists $e
@@ -22,6 +31,11 @@ sudo add-apt-repository ppa:${e}
 
 done 
 
+apt policy | grep spotify
+if [ $? != 0 ]; then
+	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+fi
 
 #arquitetura 32 bits
 sudo dpkg --add-architecture i386
@@ -29,16 +43,9 @@ sudo apt update
 sudo apt upgrade
 sudo apt --fix-broken install
 
-is_online=$(ping -c 1 -q 8.8.8.8 >&/dev/null; echo $?)
-
-if [ $is_online != 0 ]
-then
-	echo "You are offline, this script will not work."
-	exit
-fi
 
 #PACOTES
-APT_PKGS=( "snapd" "flathub" "python3.8" "default-jdk" "openjdk-8-jdk" "python3-pip" "npm" "lua" "jupyter-notebook" "love" "ffmpeg" "okular" "audacity" "transmission" "firefox" "apt-transport-https" "preload" "putty" "telegram-desktop" "discord" "xclip" "nano" "dia" "krita" "inkskape" "scribus" "git" "ppa-purge" "gufw" "xz-utils" "clamav" "font-manager" "libreoffice" "retroarch" "wget" "unzip" "bash" "atom" "featherpad")
+APT_PKGS=( "snapd" "flathub" "python3.8" "default-jdk" "openjdk-8-jdk" "python3-pip" "npm" "lua" "jupyter-notebook" "love" "ffmpeg" "okular" "audacity" "transmission" "firefox" "apt-transport-https" "preload" "putty" "telegram-desktop" "discord" "xclip" "nano" "dia" "krita" "inkskape" "scribus" "git" "ppa-purge" "gufw" "xz-utils" "clamav" "font-manager" "libreoffice" "retroarch" "wget" "unzip" "bash" "atom" "featherpad", "spotify-client")
 
 )
 
@@ -57,3 +64,15 @@ for e in ${ATOM_PKGS[@]}; do apm install $e; done
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.github.libresprite.LibreSprite
+
+
+sudo curl -fsSL https://deno.land/x/install/install.sh | sh
+
+
+#Adicionando shells que serão carregados no login.
+#TODO Adicionar
+SH_URLS=()
+if [ -d /etc/profile.d ]
+then
+	for e in ${SH_URLS[@]};do sudo wget -P /etc/profile.d $e; done
+fi
---
+++ custom_path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#DENO
+export DENO_INSTALL="${HOME}/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
