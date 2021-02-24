59ddae94e2b8327b76c8737f9b9bf32a7010dadf
Albrigs <natanfernandessantos@protonmail.com>
2021-02-24 12:36:44 -0300

    :wrench::zap:Instaladores Melhorados
    Adicionada funçao de instalacao via gdebi ao inves de dpkg, organiacao dos softwares onde 'e necess'ario adicionar ao apt o recurso especifico

--- conf.sh
+++ conf.sh
@@ -18,6 +18,32 @@ PPA_EXISTS() #remove trecho de loop se ppa já estiver instalado
 	if [ $? != 0 ]; then continue ; fi
 }
 
+APT_INSTALL()
+{
+	clear;  if ! dpkg -l | grep -q $1; then sudo apt -f -y install $1; fi
+}
+
+PKG_IN_APT()
+{
+	HAS =$(apt search $1)
+	wait $!
+	grep -w $1 $HAS
+	echo $?
+	return	
+}
+
+GDEBI_INSTALL()
+{
+	which $1
+	if [ $? != 1 ]; then
+		DEB_PATH= "~/${1}.deb"
+		wget -O  $DEB_PATH $2
+		sudo gdebi -n $DEB_PATH 
+		sudo rm -r $DEB_PATH
+	fi
+
+}
+
 
 # Tirando travas do apt
 sudo rm /var/lib/dpkg/lock-frontend
@@ -29,20 +55,28 @@ sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 241FE6973B765FAE
 PPAS=("webupd8team/atom" "lutris-team/lutris" "oguzhaninan/stacer")
 for e in ${PPAS[@]}; do clear;  PPA_EXISTS $e; sudo sudo add-apt-repository ppa:${e}; done 
 
-
-apt policy | grep dart
-if [ $? != 0 ]; then
-	sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-	sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-fi
-
-
-apt policy | grep spotify
-if [ $? != 0 ]; then
+#Spotfy
+if [ "$(PKG_IN_APT spotify)" != 0 ]; then
 	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 fi
 
+#Sublime Text
+if [ "$(PKG_IN_APT sublime-text)" != 0 ] then
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+fi 
+
+#Insomnia
+if [ "$(PKG_IN_APT insomnia)" != 0 ] then
+	echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+	wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
+
+fi 
+
+##
+#if [ "$(PKG_IN_APT )" != 0 ] then
+#fi
 
 #arquitetura 32 bits
 sudo dpkg --add-architecture i386
@@ -50,18 +84,11 @@ sudo apt update; sudo apt upgrade
 sudo apt --fix-broken install
 clear
 
-
-APT_INSTALL()
-{
-	clear;  if ! dpkg -l | grep -q $1; then sudo apt -f -y install $1; fi
-}
-
-
 #Necessidades no GalliumOS
 uname -or | grep galliu
 if [ $? != 0 ]; then
 	PKGS_SMOL=( "lightdm" "xfwm4" )
-	PKGS_REMOVE=( "lxdm" "chromiu*" "appgrid" "audaciou*" "atri*" )
+	PKGS_REMOVE=( "lxdm" "appgrid" "audaciou*" "atri*" )
 	
 	for e in ${PKGS_SMOL[@]}; do APT_INSTALL $e; done
 	for e in ${PKGS_REMOVE[@]}; do sudo apt remove -y $e; done
@@ -71,35 +98,37 @@ fi
 
 
 #PACOTES
-APT_PKGS=( "sqlitebrowser" "wmctrl" "xdotool" "snapd" "flathub" "python3.8" "default-jdk" "openjdk-8-jdk" "python3-pip" "python" "python-pip" "npm" "lua" "jupyter-notebook" "love" "ffmpeg" "okular" "audacity" "transmission" "firefox" "apt-transport-https" "preload" "putty" "telegram-desktop" "discord" "xclip" "nano" "dia" "krita" "git" "ppa-purge" "gufw" "xz-utils" "clamav" "font-manager" "retroarch" "wget" "unzip" "bash" "featherpad", "spotify-client" "dart" "sed" "stacer" )
+APT_PKGS=( "gdebi" "insomnia" "sublime-text" "sqlitebrowser" "wmctrl" "xdotool" "flathub" "nodejs" "python3.8" "default-jdk" "openjdk-8-jdk" "python3-pip" "python" "python-pip" "npm" "lua" "jupyter-notebook" "love" "ffmpeg" "okular" "audacity" "transmission" "firefox" "apt-transport-https" "preload" "putty" "telegram-desktop" "discord" "xclip" "nano" "dia" "krita" "git" "ppa-purge" "gufw" "xz-utils" "clamav" "font-manager" "retroarch" "wget" "unzip" "bash" "featherpad", "spotify-client" "dart" "sed" "stacer" )
 PIP_PKGS=( "pyinstaller" "virtualenv" "jupyterthemes" )
-SNAP_PKGS=( "hugo" "insomnia" )
 NPM_PKGS=( "npx" "nextron" )
 FLATHUB_PKGS=( 
 "com.github.libresprite.LibreSprite"
-"flathub com.github.marktext.marktext"
+"com.github.marktext.marktext"
 )
 
 
 for e in ${APT_PKGS[@]}; APT_INSTALL $e; done
 for e in ${PIP_PKGS[@]}; do clear; sudo pip3 install $e; done
-for e in ${SNAP_PKGS[@]}; do clear; sudo snap install $e; done
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; clear
 for e in ${FLATHUB_PKGS[@]}; do clear; flatpak install -y flathub $e; done
 for e in ${NPM_PKGS[@]}; do clear; sudo npm i -g $e; done; clear
 
-which discord
-if [ $? != 0 ]; then
-	sudo snap install discord
-fi
+#Discord
+wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo gdebi -n ~/discord.deb 
+sudo rm -r ~/discord.deb
 
-sudo curl -fsSL https://deno.land/x/install/install.sh | sh; clear
 
 
 #navegador min
-wget https://github.com/minbrowser/min/releases/download/v1.17.1/min_1.17.1_amd64.deb
-sudo dpkg -i min_1.17.1_amd64.deb
-sudo rm -r min_1.17.1_amd64.deb
+wget -O ~/min.deb "https://github.com/minbrowser/min/releases/download/v1.17.1/min_1.17.1_amd64.deb"
+sudo gdebi -n -i ~/min.deb
+sudo rm -r ~/min.deb
+
+#DENO
+sudo curl -fsSL https://deno.land/x/install/install.sh | sh; clear
+
+
 
 
 #Adicionando shells que serão carregados no login.
@@ -152,51 +181,15 @@ sudo rm -r gyt-master
 ROOT_SIZE=$(df | grep "/$" | cut -d " " -f 3)
 ROOT_SIZE=$(echo $(($ROOT_SIZE/1000000)))
 
-if [ $ROOT_SIZE -lt 100 ]; then
-	# Se meu root for menor que 100 gb vai instalar sublime
-	HAS_SUBLIME=$(apt search sublime-text)
-	wait $!
-	echo $HAS_SUBLIME | grep sublime-text
-
-	if [ $? != 0 ] then
-		sudo snap install sublime-text --classic
-	else
-		APT_INSTALL sublime-text
-	fi
-
-	SUBLIME_FOLDER="~/.config/sublime-text-3/Packages/"
-	SUBL_PACKAGES=(
-		"https://github.com/braver/ColorHints/archive/master.zip"
-		"https://github.com/braver/FileIcons/archive/master.zip"
-		"https://github.com/Wramberg/TerminalView/archive/master.zip"
-		"https://github.com/kiteco/KiteSublime/archive/master.zip"
-		"https://github.com/sergeche/emmet-sublime/archive/master.zip"
-		"https://github.com/jugyo/SublimeColorSchemeSelector/archive/master.zip"
-		"https://github.com/titoBouzout/SideBarEnhancements/archive/st3.zip"
-		"https://github.com/skuroda/Sublime-AdvancedNewFile/archive/master.zip"
-	)
-
-
-	for e in ${SUBL_PACKAGES[@]}; do wget -P $SUBLIME_FOLDER $e; done
-	unzip "${SUBLIME_FOLDER}/*.zip" 
-	sudo rm -r "${SUBLIME_FOLDER}/*.zip"
-
-
-else
+if [ $ROOT_SIZE -gt 100 ]; then
 	# Se meu root for maior vai instalar pacotes pesados
 	wget -nc https://dl.winehq.org/wine-builds/winehq.key
 	sudo apt-key add winehq.key
 	VERSION=$( cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f 2 )
 	sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ ${VERSION} main"
-	clear; sudo apt update; clear
-	sudo apt install --install-recommends winehq-stable
+	clear; sudo apt update -y; clear
+	sudo apt install -y --install-recommends winehq-stable
 
-
-	HEAVY_PKGS=( "atom" "libreoffice" "lutris" "steam" )
+	HEAVY_PKGS=( "libreoffice" "lutris" "steam" )
 	for e in ${HEAVY_PKGS}; do APT_INSTALL $e; done
-
-
-	ATOM_PKGS=( "emmet" "ask-stack" "git-time-machine" "highlight-selected" "advanced-open-file" "file-icons" "pigments" "color-picker" "python-snippets" "python-jedi" "language-babel" "react-es6-snippets" "react-es7-snippets" "autocomplete-modules" "data-atom" "love-ide" )
-	for e in ${ATOM_PKGS[@]}; do clear; apm install $e; done
-
 fi
